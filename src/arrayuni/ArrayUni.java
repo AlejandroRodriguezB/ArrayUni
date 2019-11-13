2a3126157f1b0891576fbbdb8abf006f486d4030
Alejandro Rodríguez <32458197+AlejandroRodriguezB@users.noreply.github.com>
2019-11-13 17:37:25 +0100

    Update ArrayUni.java

--- src/arrayuni/ArrayUni.java
+++ src/arrayuni/ArrayUni.java
@@ -18,9 +18,30 @@ public class ArrayUni {
     array[4]=5;
     int fin=array.length;
     
+        //imprime una array hecha por mi
         for(int w =0;w<fin;w++){
             System.out.println(array[w]);
         }
+        
+        //Da valor 0 una array, 1 en la diagonal y la imprime
+   array2 = new int[10][10];
+    int end=array2.length;
+   
+    //1r for para la altura
+        for(int i=0;end>i;i++){
+            //2ndo for para la longitud
+            for(int j=0;end>j;j++){
+            array2[i][j]=0;
+                if (j==i){
+                array2[i][j]=1;
+                }
+                 System.out.print(array2[i][j]);
+            }
+            System.out.println();
+            
+        }     
+        
+        
     }
     
     public static void main(String[] args) {
